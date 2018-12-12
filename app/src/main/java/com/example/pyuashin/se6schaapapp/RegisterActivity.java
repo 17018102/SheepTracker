4c4a8ca45b3ff9f37a7acc03df417153f426e72c
Ismaël Winterman <17018102@student.hhs.nl>
2018-12-12 12:50:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pyuashin/se6schaapapp/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.pyuashin.se6schaapapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final TextView tvMessage = (TextView) findViewById(R.id.tvMessage);
+        final Button btRegister = (Button) findViewById(R.id.btRegister);
+    }
+}
---
+++ app/src/main/java/com/example/pyuashin/se6schaapapp/UserAreaActivity.java
@@ -0,0 +1,20 @@
+package com.example.pyuashin.se6schaapapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class UserAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_area);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final TextView tvUserInfo = (TextView) findViewById(R.id.tvUserInfo);
+    }
+}
